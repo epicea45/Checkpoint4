900cbae468419bd52cb8dc05c1f4a16ed732ca1d
epicea45 <nadege.langrand@yahoo.fr>
2020-03-05 14:21:40 +0100

    création du composant Homepage et son style

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './HomePage.css';
+import { Link } from 'react-router-dom';
+
 
 
 const HomePage = () => {
   return (
     <div className="home">
         <h1 className="wilders">Bienvenue au Show des Wilders</h1>
+      <div className="elements">  
         <img
           className="company"
           src={require('../../assets/images/company-wilders.jpg')}
           alt="Photo de la compagnie wilders"
         />
+        <Link to="/Wilders" className="link"><img className="enter" src={require('../../assets/images/Logo.png')} alt="logo wild code school"/></Link>
+      </div>  
     </div>
   );
 };
