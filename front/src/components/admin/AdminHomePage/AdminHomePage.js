984860abdbbc585785306f4863abcf0fd2aeb47c
epicea45 <nadege.langrand@yahoo.fr>
2020-03-05 20:49:58 +0100

    modification du css

--- front/src/components/admin/AdminHomePage/AdminHomePage.js
+++ front/src/components/admin/AdminHomePage/AdminHomePage.js
@@ -19,14 +19,13 @@ const AdminHomePage = () => {
     <div className="home">
       <Link to="/" className="link"><img className="enter" src={require('../../../assets/images/Logo.png')} alt="logo wild code school"/></Link>
         <h1 className="welcome">Bienvenue Victoria, dans ton interface de suivi des Wilders !</h1>
-          <h2>Gestion des Wilders et de leurs compétences</h2>      
-          
-          <h2>Gestion des embauches de Wilders</h2>
+          <div className="recup">
           {
             contacts.map(contact => (
               <Employment {...contact} />
             ))
           }
+          </div>
     </div>
   );
 };
